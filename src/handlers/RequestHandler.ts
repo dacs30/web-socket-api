efc8d82bad74b9191c904dd9ee4157c7758e1aff
Danilo Augusto Correia da Silva <dacs2012@gmail.com>
2024-10-01 23:08:33 -0400

    request handler

--- src/handlers/RequestHandler.ts
+++ src/handlers/RequestHandler.ts
@@ -6,7 +6,7 @@ class RequestHandler implements BaseRequestHandler {
 
   constructor(socket: WebSocket) {
     this.socket = socket;
-    this.socket.addEventListener("message", (event) => this.handleMessage(event.data.toString()));
+    this.socket.addEventListener("message", (event) => this.handleMessage(event));
     this.socket.addEventListener("close", () => console.log("Client disconnected"));
     this.socket.addEventListener("error", (err) => console.error(err));
     this.socket.addEventListener("open", (message) => console.log("Client connected", message));
@@ -16,9 +16,23 @@ class RequestHandler implements BaseRequestHandler {
     throw new Error("Method not implemented");
   }
 
-  handleMessage(req: string) {
-    console.log(`Received: ${req}`);
-    this.socket.send(`Echo: ${req}`);
+  handleMessage(req: WebSocket.MessageEvent) {
+    const jsonReq = JSON.parse(req.data.toString());
+    // Would be better to create classes for each message handlers and use a factory to create the correct handler
+    // passing the socket as a parameter
+    switch (jsonReq.type) {
+      case "user_message":
+        this.socket.send(JSON.stringify({ type: "user_message", message: "User message received" }));
+        break;
+      case "system_message":
+        this.socket.send(JSON.stringify({ type: "system_message", message: "System message received" }));
+        break;
+      case "ping":
+        this.socket.send(JSON.stringify({ type: "pong", message: "Pong" }));
+        break;
+      default:
+        this.socket.send('Unknown message type');
+    }
   }
 }
 
